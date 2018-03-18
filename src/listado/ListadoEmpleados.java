6573bced9826057fe4eea3a57526dd1622090a0a
Juanjo Sierra <jjsierra103@gmail.com>
2018-03-18 13:57:07 +0100

    Funciones de comprobar DNIs repetidos y asignar nuevos, con tests unitarios

--- src/listado/ListadoEmpleados.java
+++ src/listado/ListadoEmpleados.java
@@ -1,14 +1,14 @@
 package listado;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 
@@ -33,4 +33,45 @@ public class ListadoEmpleados {
     public int obtenerNumeroEmpleadosArchivo(){
         return listadoArchivo.size();
     }
+
+    // Conocer si hay DNIs repetidos en el archivo
+    public boolean hayDnisRepetidosArchivo(){
+        int total_dnis = obtenerNumeroEmpleadosArchivo();
+        long dnis_distintos = listadoArchivo.stream().map(Empleado::obtenerDni).distinct().count();
+        return (total_dnis != dnis_distintos);
+    }
+
+    // Obtener datos de empleados con DNIs repetidos
+    public Map<String, List<Empleado>> obtenerDnisRepetidosArchivo(){
+        // Primero se obtienen los DNIs con los datos de los empleados asociados
+        Map<String, List<Empleado>> dnis = listadoArchivo.stream().collect(Collectors.groupingBy(Empleado::obtenerDni));
+
+        // Y a continuación se filtran solo aquellos DNIs que correspondan a más de un empleado, almacenándolos en un Map
+        Map<String, List<Empleado>> dnis_repetidos = dnis.entrySet().stream().filter(entrada -> entrada.getValue().size() > 1).
+                collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        return dnis_repetidos;
+    }
+
+    // Conteo de empleados con DNI repetido, haciendo uso del método anterior
+    public int contarEmpleadosDnisRepetidos(){
+        Map<String, List<Empleado>> dnis_repetidos = obtenerDnisRepetidosArchivo();
+
+        // Ahora obtenemos el número de empleados afectados, la suma de los valores del mapa
+        int empleados_repetidos = dnis_repetidos.entrySet().stream().mapToInt(entrada -> entrada.getValue().size()).sum();
+        return empleados_repetidos;
+    }
+
+    // Reparación de DNIs repetidos, generando nuevos DNIs para los empleados afectados
+    public void repararDnisRepetidos(Map<String, List<Empleado>> lista_repeticion) {
+        while (!lista_repeticion.isEmpty()) {
+            lista_repeticion.values().stream().flatMap(empleado -> empleado.stream()).forEach(empleado -> {
+                int indice_empleado = listadoArchivo.indexOf(empleado);
+                listadoArchivo.get(indice_empleado).asignarDniAleatorio();
+            });
+
+            lista_repeticion = obtenerDnisRepetidosArchivo();
+        }
+    }
+
 }
--- src/tests/ListadoEmpleadosTest.java
+++ src/tests/ListadoEmpleadosTest.java
@@ -1,8 +1,12 @@
 package tests;
 
+import listado.Empleado;
 import listado.ListadoEmpleados;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -17,5 +21,22 @@ public class ListadoEmpleadosTest {
     @Test
     public void obtenerNumeroEmpleadosArchivo() {
         assertEquals(5000, empleados.obtenerNumeroEmpleadosArchivo());
+    }
+
+    @Test
+    public void hayDnisRepetidosArchivo(){
+        assertEquals(true, empleados.hayDnisRepetidosArchivo());
+    }
+
+    @Test
+    public void contarEmpleadosDnisRepetidos(){
+        assertEquals(4, empleados.contarEmpleadosDnisRepetidos());
+    }
+
+    @Test
+    public void repararDnisRepetidos(){
+        Map<String, List<Empleado>> dnis_repetidos = empleados.obtenerDnisRepetidosArchivo();
+        empleados.repararDnisRepetidos(dnis_repetidos);
+        assertEquals(0, empleados.contarEmpleadosDnisRepetidos());
     }
 }
